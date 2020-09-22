97d879ced16b0a0a7cb5382e340e1d7cd55123b1
zeghersbruno <bruno.zeghers@gmail.com>
2020-09-22 14:21:53 +0200

    ExB4

---
+++ exB4/ExB4.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class ExB4 {
+
+    public static void main(String[] args) {
+
+        String lCode = "";
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Enter a language Code ");
+        lCode = userInput.nextLine();
+        switch (lCode) {
+            case "NL":
+                System.out.println("Goeiedag !");
+                break;
+            case "FR":
+                System.out.println("Bonjour !");
+                break;
+            case "EN":
+                System.out.println("Good Day !");
+                break;
+            default:
+                System.out.println("Asta Luego !");
+                break;
+        }
+    }
+}
