9e619d92bdaddb056d4f2f5de2efe088e160230d
zeghersbruno <bruno.zeghers@gmail.com>
2020-09-22 10:02:11 +0200

    exA1

---
+++ exA1/ExA1.java
@@ -0,0 +1,10 @@
+public class ExA1 {
+	public static void main(String [] args) {
+		String title = "Python";
+		int numberOfDays = 5;
+		double pricePerDay = 400.00;
+		boolean priorKnowledge = false;
+		System.out.println("Everything is ok for " + title + " course, duration " + numberOfDays + " days, at a price of " + pricePerDay + " EUR per day");
+		
+	}
+}
