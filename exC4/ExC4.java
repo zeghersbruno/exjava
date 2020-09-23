1bbe01af6616cab750cfc9cfa8cc24a39c3d7722
zeghersbruno <bruno.zeghers@gmail.com>
2020-09-23 08:58:05 +0200

    ExC4

---
+++ exC4/ExC4.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class ExC4 {
+    public static void main(String[] args) {
+        welcome();
+    }
+
+    public static void welcome() {
+        Scanner userInput = new Scanner(System.in);
+        System.out.println("Welcome ! ");
+        System.out.println("Would you like to be greeted again ? (Y/N)");
+        if (userInput.nextLine().equals("Y")) {
+            welcome();
+        }
+    }
+}
